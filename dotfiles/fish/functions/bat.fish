192d49938f92f960441fc1e1c5c689b77e9b32b3
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-08-20 15:50:38 +0900

    edit bat

--- dotfiles/fish/functions/bat.fish
+++ dotfiles/fish/functions/bat.fish
@@ -1,7 +1,9 @@
 function bat
-    if which docker > /dev/null
+    if which bat > /dev/null
+        eval (which bat) $argv
+    else if which docker > /dev/null
         docker run -it --rm -v (pwd):/myapp danlynn/bat $argv
     else
-        echo 'installation error `docker`'
+        echo 'installation error `bat` or `docker`'
     end
 end
